c9db9fd15df0ab05ec5d04979aa46a53c63207bb
Alexey <alexey.tikho@gmail.com>
2020-05-19 13:34:50 +0300

    v.2

---
+++ precache-manifest.a612ae2f5a41a416c77c83070f286671.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348d54960dbcd525bf8f8cad635be9f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a9d8b3a826517958076",
+    "url": "/static/css/main.840233c3.chunk.css"
+  },
+  {
+    "revision": "5109a7aeb68050e9b2e3",
+    "url": "/static/js/2.03b4f853.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.03b4f853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a9d8b3a826517958076",
+    "url": "/static/js/main.04865e12.chunk.js"
+  },
+  {
+    "revision": "f7957040b46c285b771e",
+    "url": "/static/js/runtime-main.20a50116.js"
+  },
+  {
+    "revision": "6a2316d7e0faa325729a384f9152f02e",
+    "url": "/static/media/iconMenu.6a2316d7.svg"
+  },
+  {
+    "revision": "b30ac594e0ce73375b72619371a97f4e",
+    "url": "/static/media/logo.b30ac594.svg"
+  },
+  {
+    "revision": "4958cd4741fa98020322f28ac5387e61",
+    "url": "/static/media/rect.4958cd47.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b844665e18e12e2071b0551bb9191b3.js"
+  "/precache-manifest.a612ae2f5a41a416c77c83070f286671.js"
 );
 
 self.addEventListener('message', (event) => {
