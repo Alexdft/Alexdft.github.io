557115c2598de895bcfe6fde9aec2dc5c5701233
Alexey <alexey.tikho@gmail.com>
2020-05-29 14:29:31 +0300

    fix Icon v.2

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.576109472f3607fe9b9efbbcfe4ef3d0.js"
+  "/precache-manifest.cc90bbee49f53e1c18304fb8b37ae6e4.js"
 );
 
 self.addEventListener('message', (event) => {
