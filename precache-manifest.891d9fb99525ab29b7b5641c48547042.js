00095f7a5f198a17c24c7b59d30e4f3f9d02da53
Alexey <alexey.tikho@gmail.com>
2023-06-25 16:54:38 +0300

    first commit

---
+++ precache-manifest.891d9fb99525ab29b7b5641c48547042.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5366c7624d65c39e649",
+    "url": "/static/css/main.2bce918c.chunk.css"
+  },
+  {
+    "revision": "a5366c7624d65c39e649",
+    "url": "/static/js/main.a5366c76.chunk.js"
+  },
+  {
+    "revision": "956f34403c717f948ea0",
+    "url": "/static/js/1.956f3440.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6456b39176f645824e7f7e3f4f715ecb",
+    "url": "/static/media/woodec-logo.6456b391.svg"
+  },
+  {
+    "revision": "6456b39176f645824e7f7e3f4f715ecb",
+    "url": "/static/media/woodec.6456b391.svg"
+  },
+  {
+    "revision": "611fee4c21ab201098fdd244c9dbfc4f",
+    "url": "/static/media/right_chevron_icon.611fee4c.svg"
+  },
+  {
+    "revision": "d7eb6bfddd0f385150a4c304330cd6a9",
+    "url": "/static/media/twitter.d7eb6bfd.svg"
+  },
+  {
+    "revision": "0723004230b54aa32427347f9243df38",
+    "url": "/static/media/facebook.07230042.svg"
+  },
+  {
+    "revision": "e1bb864caf37744d81e9f7c5a09e2dc7",
+    "url": "/static/media/youtube.e1bb864c.svg"
+  },
+  {
+    "revision": "1e5baaab55bb925517b88e7b4c507f05",
+    "url": "/static/media/instagram.1e5baaab.svg"
+  },
+  {
+    "revision": "f54f90fef47511720ff4e06c93d0a098",
+    "url": "/static/media/phone.f54f90fe.svg"
+  },
+  {
+    "revision": "8a9e883cb77caf187ad03f1cb180ca05",
+    "url": "/index.html"
+  }
+];
